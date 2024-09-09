1d01f55f2e24f30d77ac1e3e17fa6d52693c99f2
heim nicolas <heim.nico@gmail.com>
2024-09-09 21:59:27 +0200

    ajustements

--- jinja_cv/setup.py
+++ jinja_cv/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
    name="jinja-cv",
-   requires=[ "jsonschema", "python-dateutil", "jinja2"],
+   requires=[ "jsonschema", "jinja2"],
    version='0.1.0',
    description='CV templating engine',
+   py_modules=[],
    extras_require = {
        'dev': ['pylint', 'pytest'],
    },
